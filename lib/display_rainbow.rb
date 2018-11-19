73001c6d416f7c2a06b2a5f793b0d0e22ba06057
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-19 11:35:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "#{colors[0][0].uppercase}: #{colors[0]}, " + 
-       "#{colors[1][0].uppercase}: #{colors[1]}, " + 
+  puts "#{colors[0][0].upcase}: #{colors[0]}, " +
+       "#{colors[1][0].upcase}: #{colors[1]}, " +
 end
